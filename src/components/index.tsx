577801648c7448c15ce4da3250cfd4f8e9326ca0
kevin <kevin.rojokevin17@gmail.com>
2025-10-05 10:06:42 -0300

    Empezando con navbar. agregando botones,input,logo

--- src/components/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Navbar() {
-  return (
-    <nav>
-      <h1>ehlinsumo</h1>
-    </nav>
-  );
-}
---
+++ src/components/layout/layout/navbarLogo.tsx
@@ -0,0 +1,7 @@
+export default function Logo() {
+  return (
+    <div className="flex items-center gap-2 text-gray-900 dark:text-black">
+      <h1 className="text-xl font-bold">EHLinsumo🦜</h1>
+    </div>
+  );
+}
