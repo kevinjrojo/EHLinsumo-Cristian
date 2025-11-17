34440ac5757bafc49511a99a6b452333332c655b
kevin <kevin.rojokevin17@gmail.com>
2025-11-17 20:07:43 -0300

    Eliminar el archivo footer.tsx y comentar el código en layout.tsx para desactivar temporalmente la funcionalidad.

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,15 +1,15 @@
-"use client";
+// "use client";
 
-import Card from "../shared/card";
-import SearchBar from "./navbar/searchBar";
+// import Card from "../shared/card";
+// import SearchBar from "./navbar/searchBar";
 
-export default function MyLayout() {
-  return (
-    <main className="container mx-auto flex-1 px-4 sm:px-6 lg:px-8 py-8">
-      <div className="block sm:hidden justify-center px-3  mb-6">
-        <SearchBar />
-      </div>
-      <Card />
-    </main>
-  );
-}
+// export default function MyLayout() {
+//   return (
+//     <main className="container mx-auto flex-1 px-4 sm:px-6 lg:px-8 py-8">
+//       <div className="block sm:hidden justify-center px-3  mb-6">
+//         <SearchBar />
+//       </div>
+//       <Card />
+//     </main>
+//   );
+// }
